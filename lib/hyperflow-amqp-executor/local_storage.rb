f42c27536def27201facefb8a086a97e8bc682ed
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-09-30 11:42:59 +0200

    Fixed referencje to Dir in local_storage.

--- lib/hyperflow-amqp-executor/local_storage.rb
+++ lib/hyperflow-amqp-executor/local_storage.rb
@@ -1,7 +1,7 @@
 module Executor
   module LocalStorage
     def workdir
-      yield @job.options.workdir if @job.options.respond_to? :workdir else DIR::getwd()
+      yield @job.options.workdir if @job.options.respond_to? :workdir else Dir::getwd()
     end
   end
 end
