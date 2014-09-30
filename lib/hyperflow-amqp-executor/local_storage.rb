9573059d0ef43aba4913ef3fb7b05fbb6712b523
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-09-30 11:47:58 +0200

    Changed respond_to? invocation.

--- lib/hyperflow-amqp-executor/local_storage.rb
+++ lib/hyperflow-amqp-executor/local_storage.rb
@@ -1,7 +1,7 @@
 module Executor
   module LocalStorage
     def workdir
-      yield @job.options.respond_to? :workdir ? @job.options.workdir : Dir::getwd()
+      yield @job.options.respond_to?("workdir") ? @job.options.workdir : Dir::getwd()
     end
   end
 end
