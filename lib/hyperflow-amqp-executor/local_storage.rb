b1b57e761087b4bf434f573f4b0f4f3db8c4f59e
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-09-30 11:40:01 +0200

    Localstorage workdir defaults to CWD.

--- lib/hyperflow-amqp-executor/local_storage.rb
+++ lib/hyperflow-amqp-executor/local_storage.rb
@@ -1,7 +1,7 @@
 module Executor
   module LocalStorage
     def workdir
-      yield @job.options.workdir
+      yield @job.options.workdir if @job.options.key?(:workdir) else DIR::getwd()
     end
   end
 end
