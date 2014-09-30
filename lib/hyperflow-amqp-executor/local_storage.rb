138bb20e8fab2bd31daa9654b7f9663c9496a63e
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-09-30 11:42:15 +0200

    Fix in local storage.

--- lib/hyperflow-amqp-executor/local_storage.rb
+++ lib/hyperflow-amqp-executor/local_storage.rb
@@ -1,7 +1,7 @@
 module Executor
   module LocalStorage
     def workdir
-      yield @job.options.workdir if @job.options.key?(:workdir) else DIR::getwd()
+      yield @job.options.workdir if @job.options.respond_to? :workdir else DIR::getwd()
     end
   end
 end
