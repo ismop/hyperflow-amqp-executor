f560b678f84952bfd4475f4bf8818ee9c776c28a
tomek <tomek.bartynski@gmail.com>
2015-12-10 16:01:52 +0100

    run a job that uses DAP storage

--- test/run_job.rb
+++ test/run_job.rb
@@ -6,9 +6,37 @@ class RunJob < Test::Unit::TestCase
 
   def test_job_run
     # add data about job, storage setting is overriden by local config
-    payload = '{ "executable" : "ruby /home/servers/mr_scenario_selection/mr_worker.rb", "args" : "0004 1 FIXME-dapToken FIXME-dapLocation \"2014-06-26 06:00:12\" \"2014-06-27 11:03:47\", "inputs": ["0": "some_input"], "outputs": ["0": "some_output"], "options":{"storage":"local"}}'
-    job_data = RecursiveOpenStruct.new(JSON.parse(payload), recurse_over_arrays: true)
+    ensure_private_token
+    job_conf = {
+        executable: 'echo "ISMOP"',
+        args: '', # cmd line arg for comparator
+        options: {
+            storage: 'dap', context_id: 1, scenario_id: nil,
+            time_from: '2015-10-01 0:00:00 +0200',
+            time_to: '2015-10-01 8:00:00 +0200',
+            profile_id: 8,
+            filename_prefix: nil,
+            measurements_path: '/tmp/measurements'
+        }
+    }
+
+    job_data = RecursiveOpenStruct.new(job_conf, recurse_over_arrays: true)
     job = Executor::Job.new('doesntmatterfornow', job_data)
+    #puts job_data.executable
     puts job.run
+  end
+
+  private
+  def ensure_private_token
+    unless ENV['PRIVATE_TOKEN']
+      if File.exist? 'config.rb'
+        require_relative './config.rb'
+      end
+      unless Executor::DapStorage.const_defined? 'PRIVATE_TOKEN'
+        fail 'DAP private token is not defined. Create a test/config.rb file '\
+             'with Executor::DapStorage::PRIVATE_TOKEN = \'TOKEN\''\
+             ' or set PRIVATE_TOKEN env variable.'
+      end
+    end
   end
 end
