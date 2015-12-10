f51ab44f4de554a96636c0c7c01fc950cc1f19f3
tomek <tomek.bartynski@gmail.com>
2015-12-10 16:53:01 +0100

    writing result to DAP

---
+++ lib/hyperflow-amqp-executor/result_writer.rb
@@ -0,0 +1,15 @@
+require 'json'
+require_relative './dap_client'
+
+module ResultWriter
+  include DapClient
+
+  def write_result(result = {})
+    @conn.post do |req|
+  req.url '/api/v1/results'
+  req.headers['PRIVATE-TOKEN'] = private_token
+  req.headers['Content-Type'] = 'application/json'
+  req.body = result.to_json
+end
+  end
+end
