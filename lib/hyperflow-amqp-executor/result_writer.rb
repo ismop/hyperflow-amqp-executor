6363bf6158ff2b50cc2e9f8026df31e3645632d2
tomek <tomek.bartynski@gmail.com>
2015-12-11 16:22:26 +0100

    fixed an init bug in writer

--- lib/hyperflow-amqp-executor/result_writer.rb
+++ lib/hyperflow-amqp-executor/result_writer.rb
@@ -5,11 +5,12 @@ module ResultWriter
   include DapClient
 
   def write_result(result = {})
+    init_connection unless @conn
     @conn.post do |req|
-  req.url '/api/v1/results'
-  req.headers['PRIVATE-TOKEN'] = private_token
-  req.headers['Content-Type'] = 'application/json'
-  req.body = result.to_json
-end
+      req.url '/api/v1/results'
+      req.headers['PRIVATE-TOKEN'] = private_token
+      req.headers['Content-Type'] = 'application/json'
+      req.body = result.to_json
+    end
   end
 end
